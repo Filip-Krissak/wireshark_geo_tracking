debeddf362e2157f3f2fb7cca4740cd9e900c73e
JasBs <fifoboss007@gmail.com>
2023-08-04 00:12:27 +0200

    upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import dpkt
+import socket
+import pygeoip
+import requests
+
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
+
+def getip():
+        response = requests.get('https://api64.ipify.org?format=json')
+        ip_data = response.json()
+        public_ip = ip_data['ip']
+        return public_ip
+public_ip = getip()
+
+
+def retKML(dstip, srcip):
+    dst = gi.record_by_name(dstip)
+    src = gi.record_by_name(public_ip)
+    try:
+        dstlongitude = dst['longitude']
+        dstlatitude = dst['latitude']
+        srclongitude = src['longitude']
+        srclatitude = src['latitude']
+        kml = (
+            '<Placemark>\n'
+            '<name>%s</name>\n'
+            '<extrude>1</extrude>\n'
+            '<tessellate>1</tessellate>\n'
+            '<styleUrl>#transBluePoly</styleUrl>\n'
+            '<LineString>\n'
+            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+            '</LineString>\n'
+            '</Placemark>\n'
+        )%(dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
+        return kml
+    except:
+        return ''
+    
+
+def plotIPs(pcap):
+    kmlPts = ''
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = socket.inet_ntoa(ip.src)
+            dst = socket.inet_ntoa(ip.dst)
+            KML = retKML(dst, src)
+            kmlPts = kmlPts + KML
+        except:
+            pass
+    return kmlPts
+
+
+def main():
+    f = open('packages.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'\
+    '<Style id="transBluePoly">' \
+                '<LineStyle>' \
+                '<width>1.5</width>' \
+                '<color>501400E6</color>' \
+                '</LineStyle>' \
+                '</Style>'
+    kmlfooter = '</Document>\n</kml>\n'
+    kmldoc=kmlheader+plotIPs(pcap)+kmlfooter
+    print(kmldoc)
+
+    with open('output.kml', 'w') as kml_file:
+        kml_file.write(kmldoc)
+
+
+if __name__ == '__main__':
+    main()
